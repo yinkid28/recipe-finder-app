f27d3c263b522b21ec5e83b506959867fa63ec6f
Adeyinka ADedayo <adeyinkaadedayo1024@mail.com>
2025-12-13 07:41:22 +0100

    Force add config file

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// js/config.js
+const API_KEYS = {
+    spoonacular: '8afe0958ea5443d69c59a593fa9d59f2',
+    nutritionix: {
+        appId: 'LATER',
+        appKey: 'LATER'
+    },
+    unsplash: 'LATER'
+};
+
+export default API_KEYS;
